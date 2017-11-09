ce964ebe7fc020798f1bd5099175c03c5923b9f8
Ray Hong <rayhong@uw.edu>
2017-11-09 14:46:02 -0800

    11/09/2017 added comments on JS files for LOGIN_INIT, LOGIN_SIGNUP, and LOGIN_SIGNIN screens

--- source/js/LOGIN_SIGNUP.js
+++ source/js/LOGIN_SIGNUP.js
@@ -1,10 +1,12 @@
+// js for LOGIN_SIGNUP.html
+// Present a LOGIN_SIGNUP screen
+
 $.ajax({
 	url: "html/LOGIN_SIGNUP.html",
 	dataType: "html",
 	async: true,
 	success: function (data){
-		//If news file loaded succesfully
-		//Step 1. update UI
+		//Step 1. update UI at #canvas
 		$("#canvas").empty();
 		$("#canvas").append(data);
 		setTimeout(function(){
@@ -15,15 +17,14 @@ $.ajax({
 			var height_cont_left = $("#SCR_LOGIN_SIGNUP #left").height();
 			var height_cont = height_cont_right;
 			$("#SCR_LOGIN_SIGNUP #left").css("top", (height_cont_right-height_cont_left)/2);
-			
 			$("#SCR_LOGIN_SIGNUP").css("top", (height_window - height_cont)/2 - 30 + "px");
 			$("#SCR_LOGIN_SIGNUP").css("left", (width_window - 800)/2 + "px");
 			$(".credit").css("top", (height_window - 40) + "px");
 
 			//Step 3. Addcheck routines and event handler
 			//3-1. Check input validity
-			//DO THIS LATER -
-			//Check inputs. Chnage Sign up btn class to "btn" if everything is okay.
+			//T.B.D. Developing checking module 
+			//Check inputs. Chnage class of the Sign up btn (#LOGIN_SIGNUP_DONE) to "btn" if everything is okay.)
 			//3-2. Upload
 			$(document).on("click", "#PROFILE_UPLOAD", function(){
 				console.log("image upload handling");
@@ -32,10 +33,10 @@ $.ajax({
 			$(document).on("click", "#LOGIN_SIGNUP_DONE", function(){
 				if($(this).attr("class") == "btn"){
 					offEvents_login_signup();
-					loadScreen_login_signup($(this).attr("id"));		
+					loadScreen_login_signup($(this).attr("id"));
+					//T.B.D.: finish SIGN up call - check if the input user enter correct.
 				}
 				else{console.log("not ready for registration.");}
-				//T.B.D.
 			});
 			//3-4. Cancel
 			$(document).on("click", "#LOGIN_INIT", function(){
@@ -43,14 +44,15 @@ $.ajax({
 				offEvents_login_signup();
 				loadScreen_login_signup($(this).attr("id"));
 			});
-			$("#canvas").animate({"opacity":1}, 500);
-		}, 300);
+			$("#canvas").animate({"opacity":1}, time_scr_fadein);
+		}, time_scr_loadbuffer);
 	},
 	error: function (request, error){console.log(error)}
 });
 
+// Function: Loading different screens from LOGIN_SIGNUP.html
 function loadScreen_login_signup(screen_id){
-	$("#canvas").animate({"opacity":0}, 300, function(){
+	$("#canvas").animate({"opacity":0}, time_scr_fadeout, function(){
 		$.ajax({
 			url: "js/" + screen_id + ".js",
 			dataType: "script",
@@ -61,6 +63,8 @@ function loadScreen_login_signup(screen_id){
 	})
 }
 
+// Function: Off events, triggered once loading different screens
+// T.B.D. including any event including in this screen should be offed in this function
 function offEvents_login_signup(){
 	$(document).off("click", "#PROFILE_UPLOAD");
 	$(document).off("click", "#LOGIN_SIGNUP_DONE");
---
+++ source/js/global.js
@@ -0,0 +1,2 @@
+var time_scr_fadein = 500;
+var time_scr_fadeout = 300;
