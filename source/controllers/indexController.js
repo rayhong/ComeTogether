d1b18d4655f4bb102b52f161ff740e5c0aeb554e
tsook <kimts0830@gmail.com>
2018-01-04 13:47:18 +0900

    registration information verification (client and server-side) and display validation messages/info

--- source/controllers/indexController.js
+++ source/controllers/indexController.js
@@ -35,35 +35,66 @@ module.exports = function(app, con){
 	// post handler to store registration information in database and permanently storing profile image
 	app.post('/register', function(req, res){
 		var data = req.body;
-		// hash the password
-		bcrypt.hash(data.password, 10, function(err, hash){
-			if (err) throw err;
-			var img_dir = __dirname + "/../temp/" + data.filename;
-			var sql = "INSERT INTO users (user_id, user_pw, user_name, user_home_address, user_office_address, user_groups) VALUES ('";
-			if(data.homeaddr == "" && data.officeaddr == "")
-				sql += data.email + "', '" + hash + "', '" + data.name + "', NULL, NULL, ";
-			else if(data.homeaddr == "")
-				sql += data.email + "', '" + hash + "', '" + data.name + "', NULL, '" + data.officeaddr + "', ";
-			else if (data.officeaddr == "")
-				sql += data.email + "', '" + hash + "', '" + data.name + "', '" + data.homeaddr + "', NULL, ";
-			else
-				sql += data.email + "', '" + hash + "', '" + data.name + "', '" + data.homeaddr + "', '" + data.officeaddr + "', ";
-
-			sql += "JSON_OBJECT('type', 'user_groups', 'data', JSON_ARRAY()))"
-			// insert into table through sql query
+		// verifies the data given is valid (in case user deactivates javascript)
+		var emailRe = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
+		var pwRe = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{7,}$/
+		if(!pwRe.test(data.password) || !emailRe.test(data.email)){
+			res.send(JSON.stringify({success:false}))
+		}else{
+			var sql = "SELECT user_id FROM users WHERE user_id=" + con.escape(data.email);
 			con.query(sql, function(err, result){
-				if (err) throw err;
-				// copy file from temp to profile_imgs folder, delete temp file
-				fs.readFile(img_dir, function(err, data){
-					if (err) throw err;
-					fs.writeFile(__dirname + "/../profile_imgs/" + result.insertId + path.extname(img_dir), data, function(err){
+				// checks if email already exists
+				if(result.length > 0)
+					res.send(JSON.stringify({success: false}));
+				else{
+					// hash the password
+					bcrypt.hash(data.password, 10, function(err, hash){
 						if (err) throw err;
-						fs.unlink(img_dir, function(err){ if (err) throw err; });
+						var img_dir = __dirname + "/../temp/" + data.filename;
+
+						// create the sql query string
+						var sql = "INSERT INTO users (user_id, user_pw, user_name, user_home_address, user_office_address, user_groups) VALUES (";
+						if(data.homeaddr == "" && data.officeaddr == "")
+							sql += `${con.escape(data.email)}, '${hash}', ${con.escape(data.name)}, NULL, NULL, `;
+						else if(data.homeaddr == "")
+							sql += `${con.escape(data.email)}, '${hash}', ${con.escape(data.name)}, NULL, ${con.escape(data.officeaddr)}, `;
+						else if (data.officeaddr == "")
+							sql += `${con.escape(data.email)}, '${hash}', ${con.escape(data.name)}, ${con.escape(data.homeaddr)}, NULL, `;
+						else
+							sql += `${con.escape(data.email)}, '${hash}', ${con.escape(data.name)}, ${con.escape(data.homeaddr)}, ${con.escape(data.officeaddr)}, `;
+						sql += "JSON_OBJECT('type', 'user_groups', 'data', JSON_ARRAY()))"
+
+						// insert new user into the table
+						con.query(sql, function(err, result){
+							if (err) throw err;
+							// copy file from temp to profile_imgs folder, and delete temp file
+							fs.readFile(img_dir, function(err, data){
+								if (err) throw err;
+								fs.writeFile(__dirname + "/../profile_imgs/" + result.insertId + path.extname(img_dir), data, function(err){
+									if (err) throw err;
+									fs.unlink(img_dir, function(err){ if (err) throw err; });
+									res.send(JSON.stringify({success: true}));
+								})
+							})
+						});
 					})
-				})
+				}
 			});
-		})
+		}
 	})
+
+	// returns true to the user if email is not already in use
+	app.post('/verify_email', function(req, res){
+		var data = req.body;
+		var sql = "SELECT user_id FROM users WHERE user_id=" + con.escape(data.email);
+		con.query(sql, function(err, result){
+			if(result.length > 0){
+				res.send(JSON.stringify({success: false}));
+			}else{
+				res.send(JSON.stringify({success: true}));
+			}
+		});
+	});
 
 	// post handler for logins: checks that userid exists and if password match
 	// returns a json to user depending on verification outcome
@@ -73,7 +104,7 @@ module.exports = function(app, con){
 			res.send(JSON.stringify({success: true, issue: false}));
 		}else{
 			var data = req.body;
-			var sql = "SELECT user_id, user_pw FROM users WHERE user_id='" + data.id +"'";
+			var sql = "SELECT user_id, user_pw FROM users WHERE user_id=" + con.escape(data.id);
 			con.query(sql, function(err, result){
 				if(result.length > 0){
 					var hashed_pw = result[0].user_pw;
--- source/js/LOGIN_SIGNUP.js
+++ source/js/LOGIN_SIGNUP.js
@@ -1,6 +1,12 @@
 // js for LOGIN_SIGNUP.html
 // Present a LOGIN_SIGNUP screen
 
+// global timer variables to check when user stops typing
+var emailTimer;
+var pwTimer;
+var pwconfTimer;
+var typingInterval = 500;
+
 $.ajax({
 	url: "html/LOGIN_SIGNUP.html",
 	dataType: "html",
@@ -27,18 +33,90 @@ $.ajax({
 			//Check inputs. Chnage class of the Sign up btn (#LOGIN_SIGNUP_DONE) to "btn" if everything is okay.)
 			//3-2. Upload
 
-			// verify that all required fields are filled
+			// verify email if user stops typing for 0.5 secs
 			$("#input_email").keyup(function(){
-				verify_required();
+				$(this).val() !="" ? $(this).css("color", "#000") : $(this).css("color", "#aaa");
+				clearTimeout(emailTimer);
+				emailTimer = setTimeout(verify_email, typingInterval);
 			})
+			$("#input_email").keydown(function(e){
+				// in case user uses tab to go to the next field
+				if(e.keyCode === 9)
+					verify_email()
+				clearTimeout(emailTimer);
+			})
+
+			// verify name on keyup
 			$("#input_name").keyup(function(){
+				if($(this).val() != ""){
+					$(this).css("color", "#000");
+					$("#check_name").html("&#10004;");
+				}else{
+					$(this).css("color", "#aaa");
+					$("#check_name").html("*");
+				}
 				verify_required();
 			})
+
+			// verify pw and pwconf when user stops typing
 			$("#input_pw").keyup(function(){
-				verify_required();
+				$(this).val() !="" ? $(this).css("color", "#000") : $(this).css("color", "#aaa");
+				clearTimeout(pwTimer);
+				pwTimer = setTimeout(verify_pw, typingInterval);
 			})
+			$("#input_pw").keydown(function(e){
+				if(e.keyCode === 9)
+					verify_pw()
+				clearTimeout(pwTimer);
+			})
+
 			$("#input_pwconf").keyup(function(){
+				$(this).val() !="" ? $(this).css("color", "#000") : $(this).css("color", "#aaa");
+				clearTimeout(pwconfTimer);
+				pwconfTimer = setTimeout(verify_pwconf, typingInterval);
+			})
+			$("#input_pwconf").keydown(function(e){
+				if(e.keyCode === 9)
+					verify_pwconf()
+				clearTimeout(pwconfTimer);
+			})
+
+			// verify home and office addresses on keyup
+			$("#input_homeadd").keyup(function(){
+				if(!$("#check_homeadd").is(":checked")){
+					$(this).val()!="" ? $(this).css("color", "#000") : $(this).css("color", "#aaa");
+					verify_required();
+				}
+			})
+			$("#input_officeadd").keyup(function(){
+				if(!$("#check_officeadd").is(":checked")){
+					$(this).val()!="" ? $(this).css("color", "#000") : $(this).css("color", "#aaa");
+					verify_required();
+				}
+			})
+
+			// make home and office address inputs writeable if "Do not want to specify" boxes are not checked
+ 			$("#check_homeadd").change(function(){
 				verify_required();
+				if(!$(this).is(":checked")){
+					$("#input_homeadd").prop("readonly", false);
+					if($("#input_homeadd").val() != "")
+						$("#input_homeadd").css("color", "#000");
+				}else{
+					$("#input_homeadd").prop("readonly", true);
+					$("#input_homeadd").css("color", "#aaa");
+				}
+			})
+			$("#check_officeadd").change(function(){
+				verify_required();
+				if(!$(this).is(":checked")){
+					$("#input_officeadd").prop("readonly", false);
+					if($("#input_officeadd").val() != "")
+						$("#input_officeadd").css("color", "#000");
+				}else{
+					$("#input_officeadd").prop("readonly", true);
+					$("#input_officeadd").css("color", "#aaa");
+				}
 			})
 
 
@@ -50,23 +128,20 @@ $.ajax({
 						$("#profile_pic").attr("src", e.target.result);
 					}
 					reader.readAsDataURL(this.files[0]);
-					$("#img_upload").data("filled", true);
+					$(this).data("filled", true);
+					$("#check_upload").html("&#10004;")
 					verify_required();
 				}
 			})
-
+			// triger click in input to upload file
 			$(document).on("click", "#PROFILE_UPLOAD", function(){
-				console.log("image upload handling");
-				$("#img_upload").trigger('click'); // triger click in input to upload file
+				$("#img_upload").trigger('click'); 
 			});
+
 			//3-3. Register
 			$(document).on("click", "#LOGIN_SIGNUP_DONE", function(){
 				if($(this).attr("class") == "btn"){
-					offEvents_login_signup();
-					loadScreen_login_signup($(this).attr("id"));
-					//T.B.D.: finish SIGN up call - check if the input user enter correct.
-
-					// ajax post request to register
+					// ajax post request to register and move to next screen if success
 					post_register();
 				}
 				else{console.log("not ready for registration.");}
@@ -104,13 +179,84 @@ function offEvents_login_signup(){
 	$(document).off("click", "#LOGIN_INIT");
 }
 
+// verify email by checking with regex and checking that email is not in use
+function verify_email(){
+	var email = $("#input_email").val();
+	// regex to check email validity (could be improved)
+	var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
+	if(re.test(email)){
+		var data = {"email": email};
+		// check if email doesnt already exist in database
+		$.ajax({
+			type: 'POST',
+			url: "/verify_email",
+			data: JSON.stringify(data),
+			contentType: "application/json",
+			dataType: 'json',
+			success: function(data){
+				$("#input_email").data("verified", data.success)
+				if(data.success){
+					$("#check_email").html("&#10004;").css("color", "#fff");
+					$("#email_err").html("")
+				}else{
+					$("#check_email").html("&#10006;").css("color", "#c33");
+					$("#email_err").html("Email already in use.")
+				}
+				verify_required();
+			}
+		})
+	}else{
+		$("#input_email").data("verified", false)
+		$("#check_email").html("&#10006;").css("color", "#c33")
+		$("#email_err").html("Please enter a valid email.")
+		verify_required();	
+	}
+}
+
+
+// simply verifies password (could be improved)
+function verify_pw(){
+	var pw = $("#input_pw").val()
+	// regex to validate password: 1 lowercase, 1 uppercase, 1 digit, 7 in length
+	var re = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{7,}$/
+	var valid = re.test(pw)
+	$("#input_pw").data("verified", valid);
+	if(valid){
+		$("#check_pw").html("&#10004;").css("color", "#fff")
+		$("#pw_err").html("")
+	}else{
+		$("#check_pw").html("&#10006;").css("color", "#c33")
+		$("#pw_err").html("Your password must be 7 characters long and contain at least 1 letter and 1 digit.")
+	}
+	verify_required();
+	return valid;
+}
+
+// simply verifies password confirmation
+function verify_pwconf(){
+	var pw = $("#input_pwconf").val()
+	var valid = pw == $("#input_pw").val() && verify_pw($("#input_pw").val())
+	$("#input_pwconf").data("verified", valid);
+	if(valid){
+		$("#check_pwconf").html("&#10004;").css("color", "#fff")
+		$("#pwconf_err").html("")
+	}else{
+		$("#check_pwconf").html("&#10006;").css("color", "#c33")
+		$("#pwconf_err").html("Passwords do not match")
+	}
+	verify_required();
+}
+
 // verify that all required fields are filled
 // and change register button if they are
 function verify_required(){
-	if($("input_email").val() != "" && $("#input_name").val() != "" && 
-		$("#input_pw").val() != "" && $("#input_pwconf").val() != "" && $("#img_upload").data("filled"))
+	// checks if all necessary fields are filled or verified
+	if($("#input_email").data("verified") && $("#input_name").val() != "" && 
+		$("#input_pw").data("verified") && $("#input_pwconf").data("verified") && $("#img_upload").data("filled") &&
+		($("#check_homeadd").is(":checked") || $("#input_homeadd").val() != "") && 
+		($("#check_officeadd").is(":checked") || $("#input_officeadd").val() != "")){
 		$("#LOGIN_SIGNUP_DONE").attr("class", "btn");
-	else if($("#LOGIN_SIGNUP_DONE").attr('class') == "btn")
+	}else if($("#LOGIN_SIGNUP_DONE").attr('class') == "btn")
 		$("#LOGIN_SIGNUP_DONE").attr("class", "btn_dis");
 }
 
@@ -121,6 +267,10 @@ function post_register(){
 	var data = {"email": $("#input_email").val(), "name": $("#input_name").val(), 
 				"password": $("#input_pw").val(), "homeaddr": $("#input_homeadd").val(),
 				"officeaddr": $("#input_officeadd").val()};
+	if($("#check_homeadd").is(":checked"))
+		data.homeaddr = "";
+	if($("#check_officeadd").is(":checked"))
+		data.officeaddr = "";
 
 	// upload image and then upload data to register
 	$.ajax({
@@ -139,8 +289,14 @@ function post_register(){
 				contentType: 'application/json',
 				dataType: 'json',
 				success: function(data){
-					// do something after registration success
-					console.log("successfully registered " + $("#input_name").val());
+					// load the next page if registration finished correctly
+					if(data.success){
+						console.log("successfully registered");
+						offEvents_login_signup();
+						loadScreen_login_signup("LOGIN_SIGNUP_DONE");
+					}else{
+						console.log("failed registration")
+					}
 				}
 			});
 		}
